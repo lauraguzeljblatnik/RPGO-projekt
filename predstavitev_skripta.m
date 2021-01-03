3596489d38f15be028efe208a71e1d9f7a3fc915
janpristovnik <jan.pristovnik@gmail.com>
2021-01-03 23:02:15 +0100

    neki

--- predstavitev_skripta.m
+++ predstavitev_skripta.m
@@ -4,14 +4,18 @@ t = linspace(0,1,100);
 
 %poklikamo točke in vektorje
 T = getCoordClick();
-B = interpolateBeziere(T);
+T_pop = [0 0; T(1,:); 1 0; T(2,:)];
+
+B = interpolateBeziere(T_pop);
 plotbezier(B,t)
 
+
+
 %Klasifikacija
-M1 = T(1,:);
-M2 = T(3,:);
-M3 = T(2,:);
-M4 = T(4,:);
+M1 = T_pop(1,:);
+M2 = T_pop(3,:);
+M3 = T_pop(2,:);
+M4 = T_pop(4,:);
 [U,V] = izracunUV(M1,M2,M3,M4);
 klasifikacijaGP(U,V)
 
@@ -20,8 +24,11 @@ T1 = [0 0;
     5 1;
     1 0;
     3 4];
+
+
 B1 = interpolateBeziere(T1);
 plotbezier(B1,t);
+
 
 %Klasifikacija
 M1 = T1(1,:);
